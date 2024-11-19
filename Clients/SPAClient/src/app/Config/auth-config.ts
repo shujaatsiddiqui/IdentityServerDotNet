d79382cb12331db0b3854b5dd44c7479f3056d47
shujaatsiddiqui <shujaatsiddiqui91@gmail.com>
2024-11-19 00:01:54 -0800

    added new spa client

---
+++ Clients/SPAClient/src/app/Config/auth-config.ts
@@ -0,0 +1,13 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+  issuer: 'https://localhost:5443',
+  redirectUri: window.location.origin + '/signin-oidc',
+  postLogoutRedirectUri : window.location.origin + '/signin-oidc',
+  clientId: 'interactive',
+  responseType: 'code',
+  scope: 'openid profile el.manage',
+  showDebugInformation: true,
+  sessionChecksEnabled: true,
+  disableAtHashCheck: true,
+};
